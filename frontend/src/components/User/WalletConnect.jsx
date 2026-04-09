bd74a632e99cdc490ddf97f03d10c4211f035c2e
vinaykgowda <vinaykgowda@users.noreply.github.com>
2026-04-10 01:02:52 +0530

    fix: replace generic wallet SVG icons with proper Phantom and Solflare brand icons stored as local assets

--- frontend/src/components/User/WalletConnect.jsx
+++ frontend/src/components/User/WalletConnect.jsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from 'react';
 import { useWallet } from '../../context/WalletContext';
+import phantomIcon from '../../assets/phantom.svg';
+import solflareIcon from '../../assets/solflare.svg';
 
 const WalletConnect = () => {
   const { connect, loading, error } = useWallet();
@@ -33,12 +35,7 @@ const WalletConnect = () => {
           }`}
         >
           <div className="flex items-center">
-            <div className="bg-purple-900/50 border border-purple-700 rounded-full p-2">
-              <svg width="20" height="20" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="64" cy="64" r="64" fill="#AB9FF2"/>
-                <path d="M110.584 64.9142H99.142C99.142 41.6393 80.2229 23 56.5774 23C33.6354 23 15.1732 40.3354 14.0518 62.4271C12.8749 86.9691 33.8991 108 58.4438 108H63.43C84.2335 108 103.83 91.1286 107.761 70.8715C108.795 66.9663 110.584 66.2123 110.584 64.9142Z" fill="white"/>
-              </svg>
-            </div>
+            <img src={phantomIcon} alt="Phantom" className="w-10 h-10 rounded-full" />
             <span className="ml-3 font-medium text-green-300">Phantom</span>
           </div>
           {loading && selectedWallet === 'phantom' ? (
@@ -60,12 +57,7 @@ const WalletConnect = () => {
           }`}
         >
           <div className="flex items-center">
-            <div className="bg-orange-900/50 border border-orange-700 rounded-full p-2">
-              <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="16" cy="16" r="16" fill="#FE8101"/>
-                <path d="M22.9476 19.6948L21.4873 17.9383C21.4056 17.8397 21.3581 17.718 21.3581 17.5915V9.23024C21.3581 9.09085 21.3015 8.9572 21.2016 8.85732L19.6851 7.34082C19.5112 7.16689 19.1994 7.29204 19.1994 7.5305V8.19947C19.1994 8.2998 19.1574 8.39584 19.0827 8.46794L17.7415 9.78226C17.6337 9.88819 17.5724 10.0312 17.5724 10.1799V12.6807C17.5724 12.8063 17.526 12.927 17.4454 13.0255L16.225 14.4673C16.0484 14.6754 15.7218 14.5471 15.7218 14.2869V8.61224C15.7218 8.47375 15.665 8.3401 15.5651 8.24022L14.4279 7.1031C14.2539 6.92916 13.9422 7.05431 13.9422 7.29277V15.3238C13.9422 15.4493 13.8958 15.5701 13.8153 15.6686L12.5922 17.1123C12.4156 17.3205 12.089 17.1922 12.089 16.932V13.3221C12.089 13.1829 12.0316 13.0493 11.9317 12.9494L10.5452 11.5629C10.3713 11.389 10.0596 11.5141 10.0596 11.7526V17.2846C10.0596 17.4102 10.0131 17.5309 9.93251 17.6294L8.21155 19.6948C7.92931 20.0279 8.16558 20.5419 8.60377 20.5419H22.5554C22.9936 20.5419 23.2299 20.0279 22.9476 19.6948Z" fill="white"/>
-              </svg>
-            </div>
+            <img src={solflareIcon} alt="Solflare" className="w-10 h-10 rounded-full" />
             <span className="ml-3 font-medium text-green-300">Solflare</span>
           </div>
           {loading && selectedWallet === 'solflare' ? (
